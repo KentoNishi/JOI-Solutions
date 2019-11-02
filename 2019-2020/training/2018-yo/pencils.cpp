18628a494df435fedb199b7fbd01537effa438a3
Kento Nishi <kento24gs@outlook.com>
2019-11-02 15:27:11 -0700

    Added empty pencils.cpp

---
+++ 2019-2020/training/2018-yo/pencils.cpp
@@ -0,0 +1,14 @@
+// url
+
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    return 0;
+}
