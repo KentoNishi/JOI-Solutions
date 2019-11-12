aa5cea6ac575d1f3bc74d3a525c6980e95a75f09
Kento Nishi <kento24gs@outlook.com>
2019-11-12 09:13:47 -0800

    Unfinished plush toys solution

--- 2019-2020/training/2017-yo/plush-toys/main.cpp
+++ 2019-2020/training/2017-yo/plush-toys/main.cpp
@@ -10,17 +10,9 @@ int main() {
     int N,M;
     cin >> N >> M;
     vector<int> nums=vector<int>(N);
-    vector<pair<int,int>> counts=vector<pair<int,int>>(M);
-    // occurences, id
     for(int i=0;i<N;i++){
         cin>>nums[i];
         nums[i]--;
-        counts[nums[i]].first++;
-        counts[nums[i]].second=nums[i];
-    }
-    sort(counts.begin(),counts.end());
-    for(int i=0;i<M;i++){
-        cout << counts[i].first << " " << counts[i].second+1 << endl;
     }
     return 0;
 }
---
+++ 2019-2020/training/2017-yo/sol.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+int p[110000];
+// all the numbers
+int sum[20][110000];
+// [type][element]
+// number of times that a stuffed animal
+// of type occurs before element
+int sz[20];
+// how many times each type occurs
+int dp[1 << 20];
+// 100000000000000000000
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    for (int i = 0; i < a; i++) {
+        scanf("%d", p + i);
+        p[i]--;
+        sum[p[i]][i + 1]++;
+        sz[p[i]]++;
+    }
+    // read into the predefined arrays
+    for (int i = 0; i < b; i++){
+        for (int j = 0; j < a; j++) {
+            sum[i][j + 1] += sum[i][j];
+        }
+    }
+    // finish building the sum array so
+    // [i][j] is the number of occurences 
+    // of type i before location j
+    for (int i = 0; i < (1 << b); i++)
+        dp[i] = 999999999;
+    // set dp of all types to INF
+    dp[0] = 0;
+    for (int i = 0; i < (1 << b); i++) {
+        int pos = 0;
+        for (int j = 0; j < b; j++)
+            if (i & (1 << j))
+                pos += sz[j];
+        for (int j = 0; j < b; j++) {
+            if (i & (1 << j))
+                continue;
+            dp[i + (1 << j)] = min(dp[i + (1 << j)], dp[i] + sz[j] - sum[j][pos + sz[j]] + sum[j][pos]);
+        }
+    }
+    printf("%d\n", dp[(1 << b) - 1]);
+}
