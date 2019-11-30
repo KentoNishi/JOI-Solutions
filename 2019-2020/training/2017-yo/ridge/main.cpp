6828fd74ee2f301f8d8d56bbd4ad8df7a4e4d3aa
Kento Nishi <kento24gs@outlook.com>
2019-11-29 19:08:44 -0800

    4/5 Solution :neutral_face:

--- 2019-2020/training/2017-yo/ridge/main.cpp
+++ 2019-2020/training/2017-yo/ridge/main.cpp
@@ -3,6 +3,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int H, W;
+
+struct Pixel {
+    int h;
+    set<pair<int, int>> to;
+};
+
+vector<vector<Pixel>> grid;
+
+set<pair<int, int>> drop(int i, int j) {
+    if (i < 0 || i >= H || j < 0 || j >= W) {
+        return {};
+    }
+    if (grid[i][j].to.size() > 0) {
+        return grid[i][j].to;
+    }
+    set<pair<int, int>> canGo;
+    vector<int> d0 = {0, 0, 1, -1};
+    vector<int> d1 = {1, -1, 0, 0};
+    for (int k = 0; k < 4; k++) {
+        if (i + d0[k] < 0 || i + d0[k] >= H || j + d1[k] < 0 || j + d1[k] >= W) {
+            continue;
+        }
+        if (grid[i + d0[k]][j + d1[k]].h < grid[i][j].h) {
+            set<pair<int, int>> res = drop(i + d0[k], j + d1[k]);
+            canGo.insert(res.begin(), res.end());
+        }
+    }
+    if (canGo.size() == 0) {
+        canGo.insert(make_pair(i, j));
+    }
+    grid[i][j].to = canGo;
+    return canGo;
+}
+
 int main() {
+    cin >> H >> W;
+    grid = vector<vector<Pixel>>(H, vector<Pixel>(W));
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            cin >> grid[i][j].h;
+        }
+    }
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            if (grid[i][j].to.size() == 0) {
+                drop(i, j);
+            }
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            if (grid[i][j].to.size() > 1) {
+                ans++;
+            }
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
