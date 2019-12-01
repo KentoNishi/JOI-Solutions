273f6ada2994251d67df7563f923fade4aec8f27
Kento Nishi <kento24gs@outlook.com>
2019-11-30 20:46:22 -0800

    Finished ridge :smile:
    https://atcoder.jp/contests/joi2017yo/tasks/joi2017yo_e

--- 2019-2020/training/2017-yo/ridge/main.cpp
+++ 2019-2020/training/2017-yo/ridge/main.cpp
@@ -7,57 +7,55 @@ using namespace std;
 int H, W;
 
 struct Pixel {
-    int h;
-    set<int> to;
+    int height;
+    pair<int,int> target;
+    bool ridge=false;
 };
 
 vector<vector<Pixel>> grid;
 
-set<int> drop(int i, int j) {
-    if (i < 0 || i >= H || j < 0 || j >= W) {
-        return {};
-    }
-    if (grid[i][j].to.size() > 0) {
-        return grid[i][j].to;
-    }
-    set<int> canGo;
-    vector<int> d0 = {0, 0, 1, -1};
-    vector<int> d1 = {1, -1, 0, 0};
-    for (int k = 0; k < 4; k++) {
-        if (i + d0[k] < 0 || i + d0[k] >= H || j + d1[k] < 0 || j + d1[k] >= W) {
-            continue;
-        }
-        if (grid[i + d0[k]][j + d1[k]].h < grid[i][j].h) {
-            set<int> res = drop(i + d0[k], j + d1[k]);
-            canGo.insert(res.begin(), res.end());
-        }
-    }
-    if (canGo.size() == 0) {
-        canGo.insert(i * 1001 + j);
-    }
-    grid[i][j].to = canGo;
-    return canGo;
-}
-
 int main() {
     cin >> H >> W;
     grid = vector<vector<Pixel>>(H, vector<Pixel>(W));
+    vector<pair<int,int>> order=vector<pair<int,int>>(H*W);
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            cin >> grid[i][j].h;
+            cin >> grid[i][j].height;
+            grid[i][j].height--;
+            order[grid[i][j].height]=make_pair(i,j);
         }
     }
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            if (grid[i][j].to.size() == 0) {
-                drop(i, j);
+    int dx[]={0,0,1,-1};
+    int dy[]={1,-1,0,0};
+    for(int level=0;level<H*W;level++){
+        int i=order[level].first;
+        int j=order[level].second;
+        bool pool=true;
+        set<pair<int,int>> targets;
+        for(int d=0;d<4;d++){
+            if(i+dy[d]<H && i+dy[d]>=0 && j+dx[d]<W && j+dx[d]>=0){
+                if(grid[i+dy[d]][j+dx[d]].ridge){
+                    grid[i][j].ridge=true;
+                    break;
+                }
+                if(grid[i+dy[d]][j+dx[d]].height<grid[i][j].height){
+                    targets.insert(grid[i+dy[d]][j+dx[d]].target);
+                }
             }
         }
+        if(targets.size()==0){
+            targets.insert(make_pair(i,j));
+        }
+        if(targets.size()==1){
+            grid[i][j].target=*targets.begin();
+        }else if(targets.size()>1){
+            grid[i][j].ridge=true;
+        }
     }
-    int ans = 0;
+    int ans=0;
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            if (grid[i][j].to.size() > 1) {
+            if(grid[i][j].ridge){
                 ans++;
             }
         }
