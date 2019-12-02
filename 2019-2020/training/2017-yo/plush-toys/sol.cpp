114ecdc6a407693cf79df8f4cd2cc2ae5e771e80
Kento Nishi <kento24gs@outlook.com>
2019-12-01 22:42:34 -0800

    Still have no idea what is going on in the official solution :cry:

--- 2019-2020/training/2017-yo/plush-toys/sol.cpp
+++ 2019-2020/training/2017-yo/plush-toys/sol.cpp
@@ -1,15 +1,16 @@
 // official solution
 #include <algorithm>
+#include <iostream>
 #include <stdio.h>
 using namespace std;
 int nums[110000];
 // all the numbers
 int toLeft[20][110000];
-// [tynumse][element]
+// [type][element]
 // number of times that a stuffed animal
-// of tynumse occurs before element
+// of type occurs before element
 int quantities[20];
-// how many times each tynumse occurs
+// how many times each type occurs
 int arrangement[1 << 20];
 // 100000000000000000000
 int main() {
@@ -21,7 +22,7 @@ int main() {
         toLeft[nums[i]][i + 1]++;
         quantities[nums[i]]++;
     }
-    // read into the numsredefined arrays
+    // read into the undefined arrays
     for (int i = 0; i < b; i++) {
         for (int j = 0; j < a; j++) {
             toLeft[i][j + 1] += toLeft[i][j];
@@ -29,16 +30,23 @@ int main() {
     }
     // finish building the toLeft array so
     // [i][j] is the number of occurences
-    // of tynumse i before location j
+    // of type i before location j
     for (int i = 0; i < (1 << b); i++)
         arrangement[i] = 999999999;
-    // set arrangement of all tynumses to INF
+    // set arrangement of all types to INF
     arrangement[0] = 0;
     for (int i = 0; i < (1 << b); i++) {
+        for (int c = 0; c <= i; c++) {
+            cout << arrangement[c] << " ";
+        }
+        cout << endl;
         int position = 0;
-        for (int j = 0; j < b; j++)
-            if (i & (1 << j))
+        for (int j = 0; j < b; j++) {
+            if (i & (1 << j)) {
                 position += quantities[j];
+            }
+        }
+        cout << "When i was " << i << " position was " << position << endl;
         for (int j = 0; j < b; j++) {
             if (i & (1 << j))
                 continue;
