48a1c499b4a64154289af9ef8382bf437201841d
Kento Nishi <kento24gs@outlook.com>
2019-12-01 22:24:33 -0800

    Made the official solution slightly easier to understand

--- 2019-2020/training/2017-yo/plush-toys/main.cpp
+++ 2019-2020/training/2017-yo/plush-toys/main.cpp
@@ -10,9 +10,19 @@ int main() {
     int N,M;
     cin >> N >> M;
     vector<int> nums=vector<int>(N);
+    vector<vector<int>> toLeft=vector<vector<int>>(M,vector<int>(N+1));
+    vector<int> quantities=vector<int>(M,0);
+    vector<int> arrangement=vector<int>(M,INFINITY);
     for(int i=0;i<N;i++){
         cin>>nums[i];
         nums[i]--;
+        quantities[nums[i]]++;
+        toLeft[nums[i]][i+1]++;
+    }
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            toLeft[i][j+1]=toLeft[i][j];
+        }
     }
     return 0;
 }
--- 2019-2020/training/2017-yo/plush-toys/sol.cpp
+++ 2019-2020/training/2017-yo/plush-toys/sol.cpp
@@ -2,48 +2,48 @@
 #include <algorithm>
 #include <stdio.h>
 using namespace std;
-int p[110000];
+int nums[110000];
 // all the numbers
-int sum[20][110000];
-// [type][element]
+int toLeft[20][110000];
+// [tynumse][element]
 // number of times that a stuffed animal
-// of type occurs before element
-int sz[20];
-// how many times each type occurs
-int dp[1 << 20];
+// of tynumse occurs before element
+int quantities[20];
+// how many times each tynumse occurs
+int arrangement[1 << 20];
 // 100000000000000000000
 int main() {
     int a, b;
     scanf("%d%d", &a, &b);
     for (int i = 0; i < a; i++) {
-        scanf("%d", p + i);
-        p[i]--;
-        sum[p[i]][i + 1]++;
-        sz[p[i]]++;
+        scanf("%d", nums + i);
+        nums[i]--;
+        toLeft[nums[i]][i + 1]++;
+        quantities[nums[i]]++;
     }
-    // read into the predefined arrays
+    // read into the numsredefined arrays
     for (int i = 0; i < b; i++) {
         for (int j = 0; j < a; j++) {
-            sum[i][j + 1] += sum[i][j];
+            toLeft[i][j + 1] += toLeft[i][j];
         }
     }
-    // finish building the sum array so
+    // finish building the toLeft array so
     // [i][j] is the number of occurences
-    // of type i before location j
+    // of tynumse i before location j
     for (int i = 0; i < (1 << b); i++)
-        dp[i] = 999999999;
-    // set dp of all types to INF
-    dp[0] = 0;
+        arrangement[i] = 999999999;
+    // set arrangement of all tynumses to INF
+    arrangement[0] = 0;
     for (int i = 0; i < (1 << b); i++) {
-        int pos = 0;
+        int numsos = 0;
         for (int j = 0; j < b; j++)
             if (i & (1 << j))
-                pos += sz[j];
+                numsos += quantities[j];
         for (int j = 0; j < b; j++) {
             if (i & (1 << j))
                 continue;
-            dp[i + (1 << j)] = min(dp[i + (1 << j)], dp[i] + sz[j] - sum[j][pos + sz[j]] + sum[j][pos]);
+            arrangement[i + (1 << j)] = min(arrangement[i + (1 << j)], arrangement[i] + quantities[j] - toLeft[j][numsos + quantities[j]] + toLeft[j][numsos]);
         }
     }
-    printf("%d\n", dp[(1 << b) - 1]);
+    printf("%d\n", arrangement[(1 << b) - 1]);
 }
