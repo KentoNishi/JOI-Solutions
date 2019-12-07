17e41cab864c76026a2e7a3d47eac743ba28f64a
Kento Nishi <kento24gs@outlook.com>
2019-12-06 22:46:38 -0800

    Finished sink - 2019-yo

--- 2019-2020/training/2019-yo/sink/main.cpp
+++ 2019-2020/training/2019-yo/sink/main.cpp
@@ -1,4 +1,4 @@
-// Test case path: [C:\Users\kento\OneDrive\Files\Downloads\2019-yo-data\2019-yo-t4]
+// Test case path: [C:\Users\kento\OneDrive\Files\Documents\GitHub\JOI-Solutions\2019-2020\training\2019-yo\2019-yo-data\2019-yo-t4]
 // https://atcoder.jp/contests/joi2019yo/tasks/joi2019_yo_d
 
 #include <bits/stdc++.h>
@@ -8,18 +8,16 @@ int N;
 int main() {
     cin >> N;
     vector<int> heights = vector<int>(N);
-    map<int, int> localMins;
-    map<int, int> localMaxs;
     for (int i = 0; i < N; i++) {
         cin >> heights[i];
     }
     vector<bool> isSea = vector<bool>(N + 2);
-    isSea[0] = isSea[N] = true;
+    isSea[0] = isSea[N + 1] = true;
     for (int i = 0; i < N; i++) {
         isSea[i + 1] = heights[i] == 0;
     }
     int islandCount = 0;
-    for (int i = 1; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
         if (isSea[i - 1] && !isSea[i]) {
             islandCount++;
         }
@@ -35,14 +33,14 @@ int main() {
     int ans = islandCount;
     for (int i = 0; i < surfaces.size(); i++) {
         int index = surfaces[i].second;
-        isSea[i] = true;
-        if (isSea[i - 1] && isSea[i + 1]) {
+        isSea[index] = true;
+        if (isSea[index - 1] && isSea[index + 1]) {
             islandCount--;
         }
-        if (!isSea[i - 1] && !isSea[i + 1]) {
+        if (!isSea[index - 1] && !isSea[index + 1]) {
             islandCount++;
         }
-        if (i == surfaces.size()-1 || surfaces[i + 1] != surfaces[i]) {
+        if (i == surfaces.size() - 1 || surfaces[i + 1].first != surfaces[i].first) {
             ans = max(islandCount, ans);
         }
     }
