51abc20d2b3ff30f6bad2b8ce7af4e20f23e5d45
Kento Nishi <kento24gs@outlook.com>
2019-12-06 22:26:52 -0800

    Updated debugger configuration

--- 2019-2020/training/2019-yo/sink/main.cpp
+++ 2019-2020/training/2019-yo/sink/main.cpp
@@ -5,16 +5,6 @@
 using namespace std;
 int N;
 
-int countZeros(vector<int> heights) {
-    int sum = 0;
-    for (int i = 0; i < N; i++) {
-        if (heights[i] == 0) {
-            sum++;
-        }
-    }
-    return sum;
-}
-
 int main() {
     cin >> N;
     vector<int> heights = vector<int>(N);
@@ -23,54 +13,38 @@ int main() {
     for (int i = 0; i < N; i++) {
         cin >> heights[i];
     }
-    if (countZeros(heights) == N) {
-        cout << 0 << endl;
-        return 0;
+    vector<bool> isSea = vector<bool>(N + 2);
+    isSea[0] = isSea[N] = true;
+    for (int i = 0; i < N; i++) {
+        isSea[i + 1] = heights[i] == 0;
     }
-    int r = 0;
-    int maxSinceLast = heights[0];
-    for (int i = 1; i < N - 1; i++) {
-        maxSinceLast = max(maxSinceLast, heights[i]);
-        if (heights[i - 1] > heights[i] && heights[i + 1] > heights[i]) {
-            if (heights[i] > 0) {
-                localMins[heights[i]]++;
-            }
-            localMaxs[maxSinceLast]++;
-            maxSinceLast = 0;
-        }
-    }
-    if (maxSinceLast > 0) {
-        localMaxs[maxSinceLast]++;
-    }
-    int islandCount = heights[N - 1] > 0 ? 1 : 0;
+    int islandCount = 0;
     for (int i = 1; i < N; i++) {
-        if (heights[i - 1] > 0 && heights[i] == 0) {
+        if (isSea[i - 1] && !isSea[i]) {
             islandCount++;
         }
     }
-    cout << "Initially there are " << islandCount << endl;
-    auto minsPointer = localMins.begin();
-    auto maxsPointer = localMaxs.begin();
-    int ans = islandCount;
-    while (minsPointer != localMins.end() && maxsPointer != localMaxs.end()) {
-        if (maxsPointer->first == minsPointer->first) {
-            //cout << "Height " << maxsPointer->first << ": " << maxsPointer->second << " sank and " << minsPointer->second << " split off" << endl;
-            islandCount -= maxsPointer->second;
-            islandCount += minsPointer->second;
-            minsPointer++;
-            maxsPointer++;
-        } else {
-            if (minsPointer->first < maxsPointer->first) {
-                //cout << "Height " << minsPointer->first << ": " << minsPointer->second << " split off" << endl;
-                islandCount += minsPointer->second;
-                minsPointer++;
-            } else {
-                //cout << "Height " << maxsPointer->first << ": " << maxsPointer->second << " sank" << endl;
-                islandCount -= maxsPointer->second;
-                maxsPointer++;
-            }
+    vector<pair<int, int>> surfaces;
+    // <height,id>
+    for (int i = 0; i < N; i++) {
+        if (heights[i] > 0) {
+            surfaces.push_back(make_pair(heights[i], i + 1));
         }
-        ans = max(islandCount, ans);
+    }
+    sort(surfaces.begin(), surfaces.end());
+    int ans = islandCount;
+    for (int i = 0; i < surfaces.size(); i++) {
+        int index = surfaces[i].second;
+        isSea[i] = true;
+        if (isSea[i - 1] && isSea[i + 1]) {
+            islandCount--;
+        }
+        if (!isSea[i - 1] && !isSea[i + 1]) {
+            islandCount++;
+        }
+        if (i == surfaces.size()-1 || surfaces[i + 1] != surfaces[i]) {
+            ans = max(islandCount, ans);
+        }
     }
     cout << ans << endl;
     return 0;
