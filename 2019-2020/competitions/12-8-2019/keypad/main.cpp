578daa288c56b2a636ad95ef5404f6107a88d6c9
Kento Nishi <kento24gs@outlook.com>
2019-12-25 15:27:49 -0800

    Broken solution - does not work

--- 2019-2020/competitions/12-8-2019/keypad/main.cpp
+++ 2019-2020/competitions/12-8-2019/keypad/main.cpp
@@ -4,30 +4,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long M, R;
-vector<pair<long long, long long>> coords = {{0, 0}, {0, 1}, {1, 1}, {2, 1}, {0, 2}, {1, 2}, {2, 2}, {0, 3}, {1, 3}, {2, 3}};
+int M,R;
 
-long long tryIt(string num) {
-    //cout << "Trying " << num << endl;
-    long long prev = 0;
-    long long sum = 0;
-    for (auto &c : num) {
-        long long a = c - '0';
-        sum += abs(coords[prev].first - coords[a].first) + abs(coords[prev].second - coords[a].second) + 1;
-        prev = a;
+int getNumber(int x,int y){
+    if(x==0 && y==0){
+        return 0;
+    }else{
+        return 3*(y-1)+x+1;
     }
-    return sum;
+}
+
+pair<int,int> getCoords(int n){
+    if(n==0){
+        return {0,0};
+    }else{
+        return {(n-1)%3,(n-1)/3+1};
+    }
+}
+
+bool isValid(int x,int y){
+    if(y==0){
+        return x==0;
+    }else if(y>0 && y<=3 && x>=0 && x<=2){
+        return true;
+    }
+    return false;
 }
 
 int main() {
     cin >> M >> R;
-    // a%M == R
-    // a=M*i+R
-    int top = tryIt(to_string(R));
-    long long ans = top;
-    for (long long a = 1; a * M + R <= R * M; a++) {
-        ans = min(ans, tryIt(to_string(a * M + R)));
+    queue<tuple<int,int,int>> toCheck;
+    // remainder, current number, time taken
+    toCheck.emplace(0,0,0);
+    int dx[]={0,0,1,-1};
+    int dy[]={1,-1,0,0};
+    while(toCheck.size()>0){
+        int remainder,currentNumber,timeTaken;
+        tie(remainder,currentNumber,timeTaken)=toCheck.front();
+        toCheck.pop();
+        int newRemainder=(remainder*10+currentNumber)%M;
+        //cout << remainder << " " << currentNumber << " " << timeTaken << endl;
+        if(newRemainder==R){
+            cout << timeTaken+1 << endl;
+            return 0;
+        }
+        toCheck.emplace(newRemainder,currentNumber,timeTaken+1);
+        pair<int,int> currentCoords=getCoords(currentNumber);
+        for(int i=0;i<4;i++){
+            int x=currentCoords.first+dx[i];
+            int y=currentCoords.second+dy[i];
+            if(isValid(x,y)){
+                toCheck.emplace(remainder,getNumber(x,y),timeTaken+1);
+            }
+        }
     }
-    cout << ans << endl;
     return 0;
 }
