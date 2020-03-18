f5092d002dcb8ca3b9c79d852ac21a9178d10b9c
Kento Nishi <kento24gs@outlook.com>
2020-03-17 23:30:27 -0700

    Still can't solve :cry:

--- 2019-2020/training/2015-yo/treasures/main.cpp
+++ 2019-2020/training/2015-yo/treasures/main.cpp
@@ -4,6 +4,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Item {
+    int annaValue;
+    int brunoValue;
+};
+
+vector<Item> items;
+int N;
+
 int main() {
+    cin >> N;
+    items.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> items[i].annaValue >> items[i].brunoValue;
+    }
     return 0;
 }
