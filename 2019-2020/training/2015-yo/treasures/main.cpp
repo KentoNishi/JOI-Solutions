ef5b28ea46cb618f16e6d7751159264c37d0deba
Kento Nishi <kento24gs@outlook.com>
2020-03-18 00:37:11 -0700

    Started working on implementation - finished half enumeration

--- 2019-2020/training/2015-yo/treasures/main.cpp
+++ 2019-2020/training/2015-yo/treasures/main.cpp
@@ -10,7 +10,30 @@ struct Item {
 };
 
 vector<Item> items;
+vector<vector<Item>> permutations;
 int N;
+
+void halfEnum(int side, int lower, int upper, int index, Item total) {
+    if (index == upper) {
+        permutations[side].push_back(total);
+        return;
+    }
+    Item thisSide = total;
+    thisSide.annaValue += items[index].annaValue;
+    thisSide.brunoValue += items[index].brunoValue;
+    Item otherSide = total;
+    otherSide.annaValue -= items[index].annaValue;
+    otherSide.brunoValue -= items[index].brunoValue;
+    halfEnum(side, lower, upper, index + 1, total);
+    halfEnum(side, lower, upper, index + 1, thisSide);
+    halfEnum(side, lower, upper, index + 1, otherSide);
+}
+
+struct compare {
+    bool operator()(Item &a, Item &b) const {
+        return a.annaValue < b.annaValue;
+    }
+};
 
 int main() {
     cin >> N;
@@ -18,5 +41,12 @@ int main() {
     for (int i = 0; i < N; i++) {
         cin >> items[i].annaValue >> items[i].brunoValue;
     }
+    int leftSize = N / 2;
+    int rightSize = N - leftSize;
+    permutations.resize(2);
+    halfEnum(0, 0, N / 2, 0, {0, 0});
+    halfEnum(1, N / 2, N, 0, {0, 0});
+    sort(permutations[0].begin(), permutations[0].end(), compare());
+    sort(permutations[1].begin(), permutations[1].end(), compare());
     return 0;
 }
