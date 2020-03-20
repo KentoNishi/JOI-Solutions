4754c8e066777ae953ab855b378adc40d2cb01b1
Kento Nishi <kento24gs@outlook.com>
2020-03-19 19:17:53 -0700

    Finished treasures
    https://atcoder.jp/contests/joi2015yo/tasks/joi2015yo_f

--- 2019-2020/training/2015-yo/treasures/main.cpp
+++ 2019-2020/training/2015-yo/treasures/main.cpp
@@ -36,6 +36,38 @@ struct compare {
     }
 };
 
+int64 findAns(vector<Item> &leftSide, vector<Item> &rightSide) {
+    int64 ans = 0;
+    int64 lowerBound = 0, upperBound = 0, index = 0;
+    multiset<int64> sortedByBruno;
+    while (lowerBound < leftSide.size() && index < rightSide.size()) {
+        int64 query = rightSide[index].annaValue;
+        int64 queryMin = query - D;
+        int64 queryMax = query + D;
+        while (lowerBound < leftSide.size() && leftSide[lowerBound].annaValue < queryMin) {
+            sortedByBruno.erase(leftSide[lowerBound].brunoValue);
+            lowerBound++;
+        }
+        upperBound = max(upperBound, lowerBound);
+        while (upperBound < leftSide.size() && leftSide[upperBound].annaValue <= queryMax) {
+            sortedByBruno.insert(leftSide[upperBound].brunoValue);
+            upperBound++;
+        }
+        if (sortedByBruno.size() > 0) {
+            auto front = sortedByBruno.begin();
+            auto back = sortedByBruno.end();
+            back--;
+            int64 top = *front;
+            int64 bottom = *back;
+            int64 myValue = rightSide[index].brunoValue;
+            ans = max(ans, abs(myValue - top));
+            ans = max(ans, abs(myValue - bottom));
+        }
+        index++;
+    }
+    return ans;
+}
+
 int main() {
     cin >> N >> D;
     items.resize(N);
@@ -49,19 +81,8 @@ int main() {
     halfEnum(1, N / 2, N, {0, 0});
     sort(permutations[0].begin(), permutations[0].end(), compare());
     sort(permutations[1].begin(), permutations[1].end(), compare());
-    int64 ans = 0;
-    for (int64 i = 0; i < permutations[0].size(); i++) {
-        Item lowerBound = permutations[0][i];
-        lowerBound.annaValue -= D;
-        vector<Item> &otherSide = permutations[1];
-        Item upperBound = permutations[0][i];
-        upperBound.annaValue += D;
-        vector<Item>::iterator iter1 = lower_bound(otherSide.begin(), otherSide.end(), lowerBound, compare());
-        while (iter1 != otherSide.end() && iter1->annaValue <= upperBound.annaValue) {
-            ans = max(ans, abs(iter1->brunoValue - permutations[0][i].brunoValue));
-            iter1++;
-        }
-    }
-    cout << ans << endl;
+    vector<Item> &leftSide = permutations[0];
+    vector<Item> &rightSide = permutations[1];
+    cout << findAns(leftSide, rightSide) << endl;
     return 0;
 }
