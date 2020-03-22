76ea24f4c74e75fa327053f176ef6f7d68ab743f
Kento Nishi <kento24gs@outlook.com>
2020-03-21 18:47:39 -0700

    Finished fish

--- 2019-2020/training/2013-yo/fish/main.cpp
+++ 2019-2020/training/2013-yo/fish/main.cpp
@@ -8,18 +8,12 @@ using namespace std;
 int N, D;
 
 struct Point {
-    int x, y, z;
-    bool operator!=(const Point &other) const {
-        return !(*this == other);
+    int64 x, y, z;
+    bool operator>=(const Point &other) const {
+        return (x >= other.x && y >= other.y && z >= other.z);
     }
-    bool operator==(const Point &other) const {
-        return (x == other.x && y == other.y && z == other.z);
-    }
-    bool operator<(const Point &other) const {
-        return (x < other.x && y < other.y && z < other.z);
-    }
-    bool operator>(const Point &other) const {
-        return (x > other.x && y > other.y && z > other.z);
+    bool operator<=(const Point &other) const {
+        return (x <= other.x && y <= other.y && z <= other.z);
     }
 };
 
@@ -27,52 +21,54 @@ struct Box {
     Point p1, p2;
 };
 
-struct Overlap {
-    Box box;
-    int layers;
-};
-
-vector<Overlap> overlaps;
-
-Box getIntersection(Box b1, Box b2) {
-    Box intersection = {{0, 0, 0}, {0, 0, 0}};
-    if (b1.p1 < b2.p1 && b1.p2 > b2.p1) {
-        intersection.p1 = b2.p1;
-        intersection.p2 = b1.p2;
-    } else if (b2.p1 < b1.p1 && b2.p2 > b1.p1) {
-        intersection.p1 = b1.p1;
-        intersection.p2 = b2.p2;
-    }
-    return intersection;
+int64 getVolume(Box box) {
+    return abs(box.p1.x - box.p2.x) * abs(box.p1.y - box.p2.y) * abs(box.p1.z - box.p2.z);
 }
 
-long long getVolume(Box box) {
-    return abs(box.p1.x - box.p2.x) * abs(box.p1.y - box.p2.y) * abs(box.p1.z - box.p2.z);
+bool inside(Box in, Box out) {
+    return in.p1 >= out.p1 && in.p2 <= out.p2;
 }
 
 int main() {
     cin >> N >> D;
-    vector<Box> bounds;
+    vector<Box> bounds = vector<Box>(N);
     for (int i = 0; i < N; i++) {
-        int a, b, c, d, e, f;
+        int64 a, b, c, d, e, f;
         cin >> a >> b >> c >> d >> e >> f;
-        bounds.push_back({{a, b, c}, {d, e, f}});
-        overlaps.push_back({bounds[i], 1});
+        bounds[i] = {{a, b, c}, {d, e, f}};
     }
-    long long ans = 0;
-    for (auto &bound : bounds) {
-        for (auto &overlap : overlaps) {
-            Box intersection = getIntersection(overlap.box, bound);
-            if (intersection.p1 != intersection.p2) {
-                overlaps.push_back({intersection, overlap.layers + 1});
-                if (overlap.layers + 1 == D) {
-                    ans += getVolume(intersection);
+    vector<int64> allX = vector<int64>(N * 2);
+    vector<int64> allY = allX;
+    vector<int64> allZ = allX;
+    for (int i = 0; i < N; i++) {
+        Box &box = bounds[i];
+        allX[2 * i] = box.p1.x;
+        allY[2 * i] = box.p1.y;
+        allZ[2 * i] = box.p1.z;
+        allX[2 * i + 1] = box.p2.x;
+        allY[2 * i + 1] = box.p2.y;
+        allZ[2 * i + 1] = box.p2.z;
+    }
+    sort(allX.begin(), allX.end());
+    sort(allY.begin(), allY.end());
+    sort(allZ.begin(), allZ.end());
+    int64 ans = 0;
+    for (int i = 0; i < 2 * N - 1; i++) {
+        for (int j = 0; j < 2 * N - 1; j++) {
+            for (int k = 0; k < 2 * N - 1; k++) {
+                Box box = {{allX[i], allY[j], allZ[k]},
+                           {allX[i + 1], allY[j + 1], allZ[k + 1]}};
+                int overlap = 0;
+                for (int m = 0; m < N; m++) {
+                    if (inside(box, bounds[m])) {
+                        overlap++;
+                    }
+                }
+                if (overlap >= D) {
+                    ans += getVolume(box);
                 }
             }
         }
-    }
-    for (auto &overlap : overlaps) {
-        cout << overlap.layers << ": " << overlap.box.p1.x << "," << overlap.box.p1.y << "," << overlap.box.p1.z << " " << overlap.box.p2.x << "," << overlap.box.p2.y << "," << overlap.box.p2.z << " " << endl;
     }
     cout << ans << endl;
     return 0;
