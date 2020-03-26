9e44a580acbb40c091adb2d29518a4876cd69c56
Kento Nishi <kento24gs@outlook.com>
2020-03-25 22:44:00 -0700

    Checkpoint - made some progress with updated states

--- 2019-2020/training/2013-yo/gifts/main.cpp
+++ 2019-2020/training/2013-yo/gifts/main.cpp
@@ -7,28 +7,63 @@ using namespace std;
 
 int64 H, W, K;
 
+struct Point {
+    int64 x;
+    int64 y;
+    Point(int64 x, int64 y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+struct State {
+    Point p;
+    int64 k;
+    set<Point> pickedUp;
+};
+
+bool operator<(const Point &a, const Point &b) {
+    return (a.x == b.x ? (a.y < b.y) : (a.x < b.x));
+}
+bool operator==(const Point &a, const Point &b) {
+    return a.x == b.x && a.y == b.y;
+}
+bool operator<(const State &a, const State &b) {
+    return (a.p == b.p) ? (a.k == b.k ? (a.pickedUp < b.pickedUp) : (a.k < b.k)) : (a.p < b.p);
+}
+
 vector<vector<int64>> grid;
-vector<vector<vector<map<set<int64>, int64>>>> memo;
-vector<vector<int64>> shopID;
+map<State, int64> memo;
 
-bool inSet(set<int64> &s, int64 n) {
-    return s.find(n) != s.end();
+bool alreadyPickedUp(set<Point> &pickedUp, int64 x, int64 y) {
+    return pickedUp.find(Point(x, y)) != pickedUp.end();
 }
 
-bool inMap(map<set<int64>, int64> &m, set<int64> &s) {
-    return m.find(s) != m.end();
+bool alreadyVisited(State state) {
+    return memo.find(state) != memo.end();
 }
 
-int64 doFill(int64 x, int64 y, int64 k, set<int64> pickedUp) {
+int64 doFill(int64 x, int64 y, int64 k, set<Point> pickedUp) {
     if (x < 0 || y < 0 || x >= H || y >= W || k == -1 || grid[x][y] == -1) {
         return -1;
     }
-    if (inMap(memo[x][y][k], pickedUp)) {
-        return memo[x][y][k][pickedUp];
+    State state = {{x, y}, k, pickedUp};
+    if (alreadyVisited(state)) {
+        return memo[state];
     }
-    int64 thisStore = (inSet(pickedUp, shopID[x][y]) ? 0 : grid[x][y]);
-    set<int64> pickedBefore = pickedUp;
-    pickedUp.insert(shopID[x][y]);
+    int64 thisStore = 0;
+    if (!alreadyPickedUp(pickedUp, x, y)) {
+        thisStore = grid[x][y];
+    }
+    if (grid[x][y] > 0) {
+        pickedUp.insert({x, y});
+    }
+    for (auto iter = pickedUp.begin(); iter != pickedUp.end(); iter++) {
+        if (max(x - iter->x, 0LL) + max(y - iter->y, 0LL) > K) {
+            pickedUp.erase(iter);
+            iter--;
+        }
+    }
     vector<int64> results = {doFill(x + 1, y, k, pickedUp),
                              doFill(x, y + 1, k, pickedUp),
                              doFill(x - 1, y, k - 1, pickedUp),
@@ -40,18 +75,16 @@ int64 doFill(int64 x, int64 y, int64 k, set<int64> pickedUp) {
         }
     }
     if (x == H - 1 && y == W - 1) {
-        memo[x][y][k][pickedBefore] = max(local, grid[x][y]);
-        return memo[x][y][k][pickedBefore];
+        memo[state] = max(local, grid[x][y]);
+    } else {
+        memo[state] = local;
     }
-    memo[x][y][k][pickedBefore] = local;
-    return local;
+    return memo[state];
 }
 
 int main() {
     cin >> H >> W >> K;
     grid = vector<vector<int64>>(H, vector<int64>(W, -1));
-    shopID = grid;
-    int64 id = 0;
     for (int64 i = 0; i < H; i++) {
         for (int64 j = 0; j < W; j++) {
             char c;
@@ -62,16 +95,10 @@ int main() {
                 grid[i][j] = -1;
             } else {
                 grid[i][j] = c - '0';
-                shopID[i][j] = id++;
             }
         }
     }
-    memo = vector<vector<vector<map<set<int64>, int64>>>>(
-        H,
-        vector<vector<map<set<int64>, int64>>>(
-            W,
-            vector<map<set<int64>, int64>>(
-                K + 1)));
-    cout << doFill(0, 0, K, set<int64>()) << endl;
+    memo = map<State, int64>();
+    cout << doFill(0, 0, K, set<Point>()) << endl;
     return 0;
 }
