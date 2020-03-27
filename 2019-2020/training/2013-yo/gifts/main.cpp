4ba28b65e0803fb09f69d23e7553bd9171009a35
Kento Nishi <kento24gs@outlook.com>
2020-03-27 11:56:37 -0700

    Finished gifts
    https://atcoder.jp/contests/joi2013yo/tasks/joi2013yo_f

--- 2019-2020/training/2013-yo/gifts/main.cpp
+++ 2019-2020/training/2013-yo/gifts/main.cpp
@@ -2,81 +2,88 @@
 // https://atcoder.jp/contests/joi2013yo/tasks/joi2013yo_f
 
 #include <bits/stdc++.h>
-#define int64 long long
 using namespace std;
 
-int64 H, W, K;
+int H, W, K;
 
 struct State {
-    pair<int64, int64> p;
-    int64 k;
-    set<pair<int64, int64>> pickedUp;
+    int x;
+    int y;
+    int k;
+    vector<pair<int, int>> pickedUp;
 };
 
+bool operator<(const pair<int, int> &a, const pair<int, int> &b) {
+    return (a.first == b.first ? (a.second < b.second) : (a.first < b.first));
+}
+
+bool operator==(const pair<int, int> &a, const pair<int, int> &b) {
+    return a.first == b.first && a.second == b.second;
+}
+
 bool operator<(const State &a, const State &b) {
-    return (a.p == b.p) ? (a.k == b.k ? (a.pickedUp < b.pickedUp) : (a.k < b.k)) : (a.p < b.p);
+    return make_pair(make_pair(a.x, a.y), make_pair(a.k, a.pickedUp)) <
+           make_pair(make_pair(b.x, b.y), make_pair(b.k, b.pickedUp));
 }
 
-bool operator==(const State &a, const State &b) {
-    return a.p == b.p && a.k == b.k && a.pickedUp == b.pickedUp;
-}
+int grid[51][51];
+map<State, int> memo = map<State, int>();
 
-vector<vector<int64>> grid;
-map<State, int64> memo;
-
-bool alreadyPickedUp(set<pair<int64, int64>> &pickedUp, int64 x, int64 y) {
-    return pickedUp.find(pair<int64, int64>(x, y)) != pickedUp.end();
-}
-
-bool alreadyVisited(State state) {
+bool alreadyVisited(State &state) {
     return memo.find(state) != memo.end();
 }
 
-int64 doFill(int64 x, int64 y, int64 k, set<pair<int64, int64>> pickedUp) {
+bool alreadyPickedUp(vector<pair<int, int>> &pickedUp, int x, int y) {
+    return find(pickedUp.begin(), pickedUp.end(), make_pair(x, y)) != pickedUp.end();
+}
+
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+
+int doFill(int x, int y, int k, vector<pair<int, int>> pickedUp) {
     if (x < 0 || y < 0 || x >= H || y >= W || k == -1 || grid[x][y] == -1) {
         return -1;
     }
-    State state = {{x, y}, k, pickedUp};
+    State state = {x, y, k, pickedUp};
     if (alreadyVisited(state)) {
         return memo[state];
     }
-    int64 thisStore = 0;
-    if (!alreadyPickedUp(pickedUp, x, y)) {
-        thisStore = grid[x][y];
-    }
-    if (grid[x][y] > 0) {
-        pickedUp.insert({x, y});
-    }
-    for (auto iter = pickedUp.begin(); iter != pickedUp.end();) {
-        if (max(x - iter->first, 0LL) + max(y - iter->second, 0LL) > K) {
-            iter = pickedUp.erase(iter);
-        } else {
-            iter++;
-        }
-    }
-    vector<int64> results = {doFill(x + 1, y, k, pickedUp),
-                             doFill(x, y + 1, k, pickedUp),
-                             doFill(x - 1, y, k - 1, pickedUp),
-                             doFill(x, y - 1, k - 1, pickedUp)};
-    int64 local = -1;
-    for (int64 &result : results) {
-        if (result != -1) {
-            local = max(local, result + thisStore);
-        }
-    }
+    int ans = -INT_MAX;
     if (x == H - 1 && y == W - 1) {
-        memo[state] = max(local, grid[x][y]);
-    } else {
-        memo[state] = local;
+        ans = 0;
     }
-    return memo[state];
+    for (int d = 0; d < 4; d++) {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+        int nk = k - (d >= 2 ? 1 : 0);
+        if (nx < 0 || ny < 0 || nx >= H || ny >= W || nk == -1 || grid[nx][ny] == -1) {
+            continue;
+        }
+        vector<pair<int, int>> visited;
+        for (int i = 0; i < pickedUp.size(); i++) {
+            if (max(nx - pickedUp[i].first, 0) + max(ny - pickedUp[i].second, 0) <= nk) {
+                visited.push_back(pickedUp[i]);
+            }
+        }
+        int val = max(grid[nx][ny], 0);
+        if (alreadyPickedUp(visited, nx, ny)) {
+            val = 0;
+        }
+        if (grid[nx][ny] != -1) {
+            visited.push_back(make_pair(nx, ny));
+            sort(visited.begin(), visited.end());
+            visited.erase(unique(visited.begin(), visited.end()), visited.end());
+        }
+        ans = max(ans, doFill(nx, ny, nk, visited) + val);
+    }
+    memo[state] = ans;
+    return ans;
 }
 
 int main() {
     cin >> H >> W >> K;
-    grid = vector<vector<int64>>(H, vector<int64>(W, -1));
-    for (int64 i = 0; i < H; i++) {
-        for (int64 j = 0; j < W; j++) {
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
             char c;
             cin >> c;
             if (c == '.') {
@@ -88,7 +95,6 @@ int main() {
             }
         }
     }
-    memo = map<State, int64>();
-    cout << doFill(0, 0, K, set<pair<int64, int64>>()) << endl;
+    cout << doFill(0, 0, K, vector<pair<int, int>>()) << endl;
     return 0;
 }
