67a96812f995ecf5fee73b9cc824c6c7c86d083e
KentoNishi <kento24gs@outlook.com>
2020-09-18 14:31:40 -0700

    Finished add_sub

---
+++ 2020-2021/training/add_sub/main.cpp
@@ -0,0 +1,13 @@
+// Test case path: [path]
+// https://atcoder.jp/contests/joi2021-practice/tasks/joi2020_practice_a
+
+#include <bits/stdc++.h>
+#define int64 long long
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b) << " " << (a - b) << endl;
+    return 0;
+}
