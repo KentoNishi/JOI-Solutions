739e245702c40c757cacee9f580833c151cef26c
KentoNishi <kento24gs@outlook.com>
2020-11-04 22:28:37 -0800

    Finished sandcastle

---
+++ 2020-2021/training/2014yo/taxis/main.cpp
@@ -0,0 +1,10 @@
+// Test case path: [path]
+// https://atcoder.jp/contests/joi2014yo/tasks/joi2014yo_e
+
+#include <bits/stdc++.h>
+#define int64 long long
+using namespace std;
+
+int main() {
+    return 0;
+}
